49af954305da1f1fc35e69b6724e3c849ee659e3
chutpagludotnet <cluster@chutpaglu.net>
2025-05-29 17:38:42 +0300

    Create vars.py

---
+++ vars.py
@@ -0,0 +1,10 @@
+# @medusaXD (Telegram)
+# Add your details here and then deploy to Render as a background worker
+import os
+from os import environ
+
+API_ID = int(environ.get("API_ID", "21311268"))
+API_HASH = environ.get("API_HASH", "af66e56cee1a95ddf34b2b5d1e422b14")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8076392409:AAElwZcjC6WlGAzxFb6aTVlzEFp1ZSiTSrE")
+OWNER = int(environ.get("OWNER", "7527795504"))
+CREDIT = "@medusaXD"
